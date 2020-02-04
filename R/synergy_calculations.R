2cef2ee13e2fb25b9955147faa96b7294e86f771
Mathurin Dorel <dorel@horus.ens.fr>
2020-02-04 12:19:53 +0100

    Adapt column names to SynergyFinder package changes
    Change column names in get_synergy_table to fit new expectation from
    ReshapeData

--- R/synergy_calculations.R
+++ R/synergy_calculations.R
@@ -11,36 +11,36 @@ get_synergy_table <- function(pdata, restrict=FALSE) {
     #pigfrazd %>% filter(!grepl("1|8", Well)) %>%
     pdata = pdata %>%
         mutate(Viability=case_when(is.infinite(Viability)&Viability<0~0, is.infinite(Viability)&Viability>0~1, TRUE~Viability)) %>% # Remove infinite
-        mutate(response = 100*Viability) # Convert Viability in ~[0,1] to a reponse in ~[0,100]
-    if (restrict) { # Scale the response strictly between 0 and 100
+        mutate(Response = 100*Viability) # Convert Viability in ~[0,1] to a reponse in ~[0,100]
+    if (restrict) { # Scale the Response strictly between 0 and 100
         pdata = pdata %>%
-            mutate(response = case_when(response<0~0, response>100~100, TRUE~response))
+            mutate(Response = case_when(Response<0~0, Response>100~100, TRUE~Response))
     }
 
     # Convert the treatments to 2 columns with the names expected by synergyfinder
-    treatments = str_split_fixed(pdata$Treatment, "\\+", 2) %>% as_tibble %>% rename(Drug1=V1, Drug2=V2) %>% separate(Drug1, c("drug_row", "conc_r"), "_") %>% separate(Drug2, c("drug_col", "conc_c"), "_")
-    drugs_col = treatments %>% distinct(drug_col) %>% pull(drug_col)
-    drugs_row = treatments %>% distinct(drug_row) %>% pull(drug_row)
+    treatments = str_split_fixed(pdata$Treatment, "\\+", 2) %>% as_tibble %>% rename(Drug1=V1, Drug2=V2) %>% separate(Drug1, c("DrugRow", "ConcRow"), "_") %>% separate(Drug2, c("DrugCol", "ConcCol"), "_")
+    drugs_col = treatments %>% distinct(DrugCol) %>% pull(DrugCol)
+    drugs_row = treatments %>% distinct(DrugRow) %>% pull(DrugRow)
     row_drug = find_first_drug(drugs_row, "DMSO")
     col_drug = find_first_drug(drugs_col, c(row_drug, "DMSO"))
     treatments %>% apply(1, function(x){
-                            if(x["drug_row"] == "DMSO") {
-                                 x[c("drug_row", "conc_r")]=c("", NA)
-                            } else if(x["drug_row"] %in% c(col_drug)) {
-                                x[c("drug_col", "conc_c")]=x[c("drug_row", "conc_r")]
-                                x[c("drug_row", "conc_r")]=c("", NA)
+                            if(x["DrugRow"] == "DMSO") {
+                                 x[c("DrugRow", "ConcRow")]=c("", NA)
+                            } else if(x["DrugRow"] %in% c(col_drug)) {
+                                x[c("DrugCol", "ConcCol")]=x[c("DrugRow", "ConcRow")]
+                                x[c("DrugRow", "ConcRow")]=c("", NA)
                             }
-                            return(x) }) %>% t %>% as.tibble %>% mutate(drug_row=row_drug, drug_col=col_drug) %>%
-         mutate(conc_r=case_when(is.na(conc_r)~"0nM", TRUE~conc_r), conc_c=case_when(is.na(conc_c)~"0nM", TRUE~conc_c)) %>%
-         mutate(conc_r = concentration_values(conc_r)*1e9, conc_r_unit = "nM", conc_c = concentration_values(conc_c)*1e9, conc_c_unit = "nM") -> treatments
+                            return(x) }) %>% t %>% as.tibble %>% mutate(DrugRow=row_drug, DrugCol=col_drug) %>%
+         mutate(ConcRow=case_when(is.na(ConcRow)~"0nM", TRUE~ConcRow), ConcCol=case_when(is.na(ConcCol)~"0nM", TRUE~ConcCol)) %>%
+         mutate(ConcRow = concentration_values(ConcRow)*1e9, ConcRowUnit = "nM", ConcCol = concentration_values(ConcCol)*1e9, ConcColUnit = "nM") -> treatments
 
     synergy_data = pdata %>%
         bind_cols(treatments) %>% # Add the treatment columns
-        group_by(drug_col, conc_c, drug_row, conc_r) %>%
-        summarize(response=mean(response)) %>% mutate(block_id=1, conc_c_unit="nM", conc_r_unit="nM") %>% # Make sure each concentration combination corresponds to 1 data point (not done earlier because controls can have different DMSO concentration thus different names but all correspond to the same drug combination of 0)
-        mutate(block_id=1) %>%
+        group_by(DrugCol, ConcCol, DrugRow, ConcRow) %>%
+        summarize(Response=mean(Response)) %>% mutate(BlockId=1, ConcColUnit="nM", ConcRowUnit="nM") %>% # Make sure each concentration combination corresponds to 1 data point (not done earlier because controls can have different DMSO concentration thus different names but all correspond to the same drug combination of 0)
+        mutate(BlockID=1) %>%
         ungroup() %>%
-        select(block_id, drug_col, conc_c, conc_c_unit, drug_row, conc_r, conc_r_unit, response) # Sanity check that all column expected by synergyfinder are present
+        select(BlockID, DrugCol, ConcCol, ConcColUnit, DrugRow, ConcRow, ConcRowUnit, Response) # Sanity check that all column expected by synergyfinder are present
 
     return(synergy_data)
 }
